5256668ff784b4ad87f3db5efea75e5325f7c012
Mark <mark.vale@torontofilmschool.ca>
2025-03-29 12:38:51 -0600

    Added singleton, adjusted input manager to work with singleton script

--- Assets/_Scripts/Input/InputManager.cs
+++ Assets/_Scripts/Input/InputManager.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.Rendering.DebugUI;
 
-public class InputManager: MonoBehaviour
+public class InputManager: Singleton<InputManager>
 {
     [SerializeField] GameObject player;
 
@@ -11,11 +10,14 @@ public class InputManager: MonoBehaviour
     private InputAction touchPosition;
     private InputAction touchPress;
 
-    private void Awake()
+
+    protected override void Awake()
     {
+        base.Awake();
         playerInput = GetComponent<PlayerInput>();
         touchPosition = playerInput.actions.FindAction("TouchPosition");
         touchPress = playerInput.actions.FindAction("TouchPress");
+        Debug.Log("InputManager Loaded");
     }
 
     private void OnEnable()
@@ -30,11 +32,16 @@ public class InputManager: MonoBehaviour
 
     private void TouchPressed(InputAction.CallbackContext context)
     {
-        Vector3 position = Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>());    
-        position.z = player.transform.position.z;
-        
-        player.transform.position = position;
-    }   
+        Vector3 position = Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>());   
+        position.z = Camera.main.nearClipPlane;
+        //position.z = player.transform.position.z;        
 
+        RaycastHit2D hit2D = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>()), Vector2.zero);
+        if(hit2D.collider != null)
+        {
+            Debug.Log(hit2D.collider.transform.gameObject.name);
+        }
+
+    }   
 }
 
