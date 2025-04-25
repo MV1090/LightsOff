d89d05ba7c24f97249e8345efe6187b6a7ce015a
Mark <mark.vale@torontofilmschool.ca>
2025-04-25 15:33:40 -0600

    Set up audio manager

--- Assets/_Scripts/Menus/GameMenu.cs
+++ Assets/_Scripts/Menus/GameMenu.cs
@@ -1,26 +1,72 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : BaseMenu
 {
+    [Header("Score")]
+    [SerializeField] TMP_Text scoreText;
+    [SerializeField] Slider timerSlider;
+
+    [SerializeField] Gradient colorGradient;
+
+    private float startTime;
     public override void InitState(MenuManager ctx)
     {
         base.InitState(ctx);
         state = MenuManager.MenuStates.GameMenu;
+
+        if (scoreText)
+        {
+            GameManager.Instance.OnScoreValueChange.AddListener(UpdateScoreText);
+            scoreText.text = GameManager.Instance.Score.ToString();
+        }
     }
 
     public override void EnterState()
     {        
         base.EnterState();
-        GameTypeManager.Instance.SetGridActive();
+
+        if (GameModeManager.Instance.GetCurrentGameMode() != GameModeManager.GameModes.BeatTheClock)
+            timerSlider.gameObject.SetActive(false);
+        else
+        {
+            startTime = GameManager.Instance.GetCurrentTime();
+            timerSlider.maxValue = startTime;   
+            timerSlider.gameObject.SetActive(true);
+        }
         
+
+        GameTypeManager.Instance.SetGridActive();        
         GameManager.Instance.ResetGame();
         Time.timeScale = 1.0f;
     }
 
     public override void ExitState()
     {
-        base.ExitState();
+        base.ExitState();      
+
         LightManager.Instance.ResetPlayableLights();
         Time.timeScale = 0.0f;
-    }   
+    }
+
+    private void FixedUpdate()
+    {
+        if (!timerSlider.IsActive())
+            return;
+
+        UpdateTimerBar(GameManager.Instance.GetCurrentTime());
+    }
+
+    void UpdateScoreText(int value)
+    {
+        scoreText.text = value.ToString();
+    }
+
+    void UpdateTimerBar(float value)
+    {
+        Color newColor = colorGradient.Evaluate(value/startTime);
+        timerSlider.image.color = newColor;
+        timerSlider.value = value;        
+    }
 }
