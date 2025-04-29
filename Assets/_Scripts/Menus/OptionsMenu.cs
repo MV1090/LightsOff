5381dd92e5b48be080393d563bdf985c5078bfaa
Mark <mark.vale@torontofilmschool.ca>
2025-04-29 11:44:35 -0600

    Linked up sound buttons

--- Assets/_Scripts/Menus/OptionsMenu.cs
+++ Assets/_Scripts/Menus/OptionsMenu.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionsMenu : BaseMenu
 
 {
-
-    [SerializeField] Button music;
-    [SerializeField] Button soundfx;
-    [SerializeField] Button accessibility;
+    
+    [SerializeField] Toggle music;
+    [SerializeField] Toggle soundfx;
+    [SerializeField] Toggle accessibility;
     [SerializeField] Slider activeLight;
     [SerializeField] Slider warningLight;
     [SerializeField] Image light1;
     [SerializeField] Image light2;
 
-
+    public AudioMixer audioMixer;
     public Gradient colorGradient;
 
     Button activeButton;
@@ -26,6 +27,9 @@ public class OptionsMenu : BaseMenu
 
         activeLight.onValueChanged.AddListener(UpdateActiveColor);
         warningLight.onValueChanged.AddListener(UpdateWarningColor);
+
+        soundfx.onValueChanged.AddListener(SoundFxToggle);
+        music.onValueChanged.AddListener(MusicToggle);
 
         activeLight.value = 0.5f;
         warningLight.value = 0.01f;
@@ -68,13 +72,22 @@ public class OptionsMenu : BaseMenu
         }       
     }
 
-    public void MusicToggle()
+    public void MusicToggle(bool isOn)
     {
+        if (!music.isOn)
+            audioMixer.SetFloat("musicVolume", -80.0f);
+        else
+            audioMixer.SetFloat("musicVolume", 0.0f);
         Debug.Log("Music Toggle On/Off");
     }
 
-    public void SoundFxToggle()
+    void SoundFxToggle(bool isOn)
     {
+        if(!isOn)
+            audioMixer.SetFloat("SFXVolume", -80.0f);
+        else
+            audioMixer.SetFloat("SFXVolume", 0.0f);
+
         Debug.Log("Sound FX Toggle On/Off");
     }
 
